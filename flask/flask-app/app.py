ca67724aac638195dfb26de68ee5360817a76303
EugeneLantz <tumbmalph@gmail.com>
2026-02-11 22:24:51 +0700

    Add files via upload

---
+++ flask/flask-app/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "OK - Flask running on WEB2 Docker"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
